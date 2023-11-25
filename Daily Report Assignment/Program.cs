fafd59b66ab3f0c11bddbf5d3b8f552704f5b90e
Nicolas Sloan <NicolasESloan@gmail.com>
2023-11-25 14:25:44 -0800

    Daily Student Report Assignment

---
+++ Daily Report Assignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Daily_Report_Assignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("--- THE TECH ACADEMY ---\n");
+            Console.WriteLine("Student Daily Report:\n");
+
+
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("\nWhat course are you on?");
+            string course = Console.ReadLine();
+
+            // Gets a string value from the user and explictly converts it into an unsigned short
+            Console.WriteLine("\nWhat page number");
+            ushort pageNum = Convert.ToUInt16(Console.ReadLine());
+
+            // Gets a string value from the user and explictly converts it into a boolean
+            Console.WriteLine("\nDo you need help with anything? Please answer \"True\" or \"False\".");
+            bool needsHelp = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("\nWere there any positive experiences you'd like to share? Please give specifics.");
+            string posXP = Console.ReadLine();
+
+            Console.WriteLine("\nIs there any other feedback you'd like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+
+            // Gets a string value from the user and explictly converts it into a byte
+            Console.WriteLine("\nHow many hours did you study today?");
+            byte hoursStudied  = Convert.ToByte(Console.ReadLine());
+
+            //Exit message
+            Console.WriteLine("\n\nthank you for your answers. an instructor will respond to this shortly. have a great day!");
+            Console.ReadLine();
+        }
+    }
+}
