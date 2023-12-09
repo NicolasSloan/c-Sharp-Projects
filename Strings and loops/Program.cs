a9714b44d1cf50aee7568a4b832c73f4e61ee7f7
Nicolas Sloan <NicolasESloan@gmail.com>
2023-12-09 14:44:42 -0800

    Strings and loops
    Practice using strings and loops

---
+++ Strings and loops/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _6PartAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "nic", "michael", "joesph", "sally" };
+
+            Console.WriteLine("Please enter a last name");
+            string userInput = Convert.ToString(Console.ReadLine());
+            
+            for (int i = 0; i < names.Length; i++)
+            {
+                names[i] = names[i] + " " + userInput;
+            }
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+        }
+    }
+}
